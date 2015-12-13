6a6bc22d2221f8d5eadd48663545093b41d27997
Armando <apiconz@gmail.com>
2015-12-13 13:33:52 -0500

    New changelist

--- app/src/main/java/pe/apiconz/android/sanisidromovil/application/SanIsidroMovilApp.java
+++ app/src/main/java/pe/apiconz/android/sanisidromovil/application/SanIsidroMovilApp.java
@@ -2,13 +2,32 @@ package pe.apiconz.android.sanisidromovil.application;
 
 import android.app.Application;
 
+import com.firebase.client.Firebase;
+
 /**
  * Created by Armando on 12/12/2015.
  */
 public class SanIsidroMovilApp extends Application {
 
+    private String FIREBASE_URL = "https://demohacksi.firebaseio.com/";
+    private String FIREBASE_CHILD_ACTIVITIES = "results";
+
+    private Firebase firebase;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        setupFirebase();
     }
+
+    private void setupFirebase() {
+        Firebase.setAndroidContext(this);
+        Firebase.getDefaultConfig().setPersistenceEnabled(true);
+        firebase = new Firebase(FIREBASE_URL);
+    }
+
+    public Firebase getFirebaseReferenceForActivities() {
+        return firebase.child(FIREBASE_CHILD_ACTIVITIES);
+    }
+
 }
